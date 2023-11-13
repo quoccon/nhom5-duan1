c0886abac41478b44f6e67f0c9c4adf6181d5da6
admin <daovanvinh0920@gmail.com>
2023-11-13 22:35:10 +0700

    dao and adapter

--- app/src/main/java/com/example/duan1/Model/NhanVienModel.java
+++ app/src/main/java/com/example/duan1/Model/NhanVienModel.java
@@ -3,6 +3,58 @@ package com.example.duan1.Model;
 public class NhanVienModel {
     private int MaNV;
     private String tenNV;
-    private int NamSinh;
+    private String NamSinh;
+    private String TaiKhoan;
+    private String MatKhau;
 
+    public NhanVienModel() {
+    }
+
+    public NhanVienModel(int maNV, String tenNV, String namSinh, String taiKhoan, String matKhau) {
+        MaNV = maNV;
+        this.tenNV = tenNV;
+        NamSinh = namSinh;
+        TaiKhoan = taiKhoan;
+        MatKhau = matKhau;
+    }
+
+    public int getMaNV() {
+        return MaNV;
+    }
+
+    public void setMaNV(int maNV) {
+        MaNV = maNV;
+    }
+
+    public String getTenNV() {
+        return tenNV;
+    }
+
+    public void setTenNV(String tenNV) {
+        this.tenNV = tenNV;
+    }
+
+    public String getNamSinh() {
+        return NamSinh;
+    }
+
+    public void setNamSinh(String namSinh) {
+        NamSinh = namSinh;
+    }
+
+    public String getTaiKhoan() {
+        return TaiKhoan;
+    }
+
+    public void setTaiKhoan(String taiKhoan) {
+        TaiKhoan = taiKhoan;
+    }
+
+    public String getMatKhau() {
+        return MatKhau;
+    }
+
+    public void setMatKhau(String matKhau) {
+        MatKhau = matKhau;
+    }
 }
